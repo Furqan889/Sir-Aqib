83585d783d19bbfa0cada53cb0074e310e5714a8
Furqan889 <furqansoftskills@gmail.com>
2026-04-18 18:35:39 +0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.adminToken || req.session.token;
+
+  if (!token) {
+    return res.redirect('/login');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (error) {
+    res.clearCookie('adminToken');
+    req.session.destroy();
+    return res.redirect('/login');
+  }
+};
+
+module.exports = { verifyToken };
